6030905e535cc6484f8437f889fa8bf273f4cfcd
emnic <emilnicklasson@hotmail.com>
2017-02-20 21:02:49 +0100

    Added more tests for config

--- test/features/configurations.feature
+++ test/features/configurations.feature
@@ -10,3 +10,40 @@ Feature: Configuration of devices
         When I click the button to add a new device 
         And input all nessescary information and press save
 		Then the new device is added and displayed on the settings devices dashboard
+
+    Scenario Outline: Save or Cancel add device
+        Given the add device page is displayed
+        When the "<button>" is pressed
+        Then the "<page>" is displayed
+            Examples:
+                | button |        page       |
+                |  Save  | Dashboard Devices |
+                | Cancel | Dashboard Devices |
+
+    Scenario: Edit device
+        Given there is at least one device added
+        When I click the button to edit the device
+        Then the device edit page is displayed
+
+    Scenario Outline: Save or Cancel edit device
+        Given the edit device page is displayed
+        When the "<button>" is pressed
+        Then the "<page>" is displayed
+            Examples:
+                | button |        page       |
+                |  Save  | Dashboard Devices |
+                | Cancel | Dashboard Devices |
+
+    Scenario: Delete device
+        Given the edit device page is displayed
+        When I click the button to delete the device
+        Then the user needs to confirm the deletion
+
+    Scenario Outline: Save or Cancel delete device
+        Given the delete device page is displayed
+        When the "<button>" is pressed
+        Then the "<page>" is displayed
+            Examples:
+                | button |       page        |
+                |   Yes  | Dashboard Devices |
+                |   No   |    Edit Grill     |
--- test/features/step_definitions/configurations.js
+++ test/features/step_definitions/configurations.js
@@ -3,6 +3,8 @@ var chai_as_promised = require('chai-as-promised');
 chai.use(chai_as_promised);
 var expect = chai.expect;
 
+var buttonText = '';
+
 module.exports = function() {
     this.Given(/^I'm in the settings menu for devices$/, function (callback) {
      browser.get('http://localhost:8100/#/page1/dashboarddevices');
@@ -34,4 +36,69 @@ module.exports = function() {
 
         expect(selectedElement.isPresent()).to.eventually.equal(true, "This text is not present in page.").and.notify(callback);
     });
+
+    this.Given(/^the add device page is displayed$/, function (callback) {
+        element(by.linkText('Add Device')).click().then(function(){
+          callback();
+        });
+    });
+        
+    this.When(/^the "([^"]*)" is pressed$/, function (button, callback) {
+        element(by.linkText(button)).click().then(function(){
+          callback();
+        });
+    });        
+
+    this.Then(/^the "([^"]*)" is displayed$/, function (page, callback) {
+        expect(browser.getTitle()).to.eventually.equal(page).and.notify(callback);
+    });
+
+    this.Given(/^there is at least one device added$/, function (callback) {
+        var device_name = element(by.id('dashboard-button1'));
+
+        callback();
+    });
+
+    this.When(/^I click the button to edit the device$/, function (callback) {
+        var devices = element.all(by.repeater('device in devices'));
+
+        devices.get(0).element(by.id('dashboard-button1')).click();
+
+        callback();
+    });
+
+    this.Given(/^the edit device page is displayed$/, function (callback) {
+        var devices = element.all(by.repeater('device in devices'));
+
+        devices.get(0).element(by.id('dashboard-button1')).click();
+        
+        expect(browser.getTitle()).to.eventually.equal('Edit Grill').and.notify(callback);
+        callback();
+    });
+
+    this.Then(/^the device edit page is displayed$/, function (callback) {
+        expect(browser.getTitle()).to.eventually.equal('Edit Grill').and.notify(callback);
+    });
+
+    this.When(/^I click the button to delete the device$/, function (callback) {
+        element(by.linkText('Remove')).click().then(function(){
+          callback();
+        });
+    });
+
+    this.Then(/^the user needs to confirm the deletion$/, function (callback) {
+        expect(browser.getTitle()).to.eventually.equal('Delete Device').and.notify(callback);
+    });
+
+    this.Given(/^the delete device page is displayed$/, function (callback) {
+        var devices = element.all(by.repeater('device in devices'));
+
+        devices.get(0).element(by.id('dashboard-button1')).click();
+        
+        expect(browser.getTitle()).to.eventually.equal('Edit Grill');
+        
+        element(by.linkText('Remove')).click().then(function(){
+          callback();
+        });
+    });   
 }
