809e8890c07920efc86c0ee860d8e69501240e61
emnic <emilnicklasson@hotmail.com>
2017-02-23 23:24:02 +0100

    Added tests for set temperature functionality

--- test/features/deviceConfiguration.feature
+++ test/features/deviceConfiguration.feature
@@ -5,14 +5,21 @@ Feature: Device Configuration
     Background:
         Given the device dashboard is displayed
 
-    @wip
+
     Scenario: Set target temp by manually inputting number
         Given the temperature sensor config menu is displayed
         When I click on the target temperature 
         Then it is possible to manually input the desired temperature
 
-    @wip
+
     Scenario: Set target temp by using slider
         Given the temperature sensor config menu is displayed
         When I adjust the slider
+        Then the temperature is updated
+
+    @wip
+    Scenario: Set target temp using list
+        Given the temperature sensor config menu is displayed
+        When I select a meat category
+        And then selects a meat type
         Then the temperature is updated
--- test/features/step_definitions/deviceConfiguration.js
+++ test/features/step_definitions/deviceConfiguration.js
@@ -3,6 +3,17 @@ var chai_as_promised = require('chai-as-promised');
 chai.use(chai_as_promised);
 var expect = chai.expect;
 
+// Help function for dropdowns
+var selectDropdownbyNum = function ( element, optionNum ) {
+    if (optionNum){
+      var options = element.all(by.tagName('option'))   
+        .then(function(options){
+          options[optionNum].click();
+        });
+    }
+};
+
+
 module.exports = function() {
     this.Given(/^the device dashboard is displayed$/, function (callback) {
         browser.get('http://localhost:8100/#/page1/dashboard');
@@ -50,5 +61,17 @@ module.exports = function() {
         slider.getAttribute('value').then(function(sliderValue) {
             expect(temp_input.getAttribute('value')).to.eventually.equal(sliderValue).and.notify(callback);   
         });
-    });                                  
+    }); 
+
+    this.When(/^I select a meat category$/, function (callback) {
+        var selectElement = element(by.id('meatCatDropdown'));
+        selectDropdownbyNum(selectElement,1)
+        expect(element(by.id('meatCatDropdown')).$('option:checked').getText()).to.eventually.equal('Beef').and.notify(callback);
+    });
+
+    this.When(/^then selects a meat type$/, function (callback) {
+        var selectElement = element(by.id('meatTypeDropdown'));
+        selectDropdownbyNum(selectElement,2)
+        expect(element(by.id('meatTypeDropdown')).$('option:checked').getText()).to.eventually.equal('Entrecote').and.notify(callback);
+    });                                     
 }
